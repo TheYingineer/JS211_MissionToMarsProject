9107be8a3f8a065134d74b191e8880bd66e39302
TheYingineer <ying.yan.huang87@gmail.com>
2022-05-11 22:50:36 -0500

    not sure if it updated correctly

--- main.js
+++ main.js
@@ -11,9 +11,60 @@ const jobTypes = {
 
 // Your code will go here
 
+class CrewMember {
+  constructor(name, job, specialSkill, ship){
+  this.name = name;
+  this.job = job;
+  this.specialSkill = specialSkill
+  this.ship = ship
+  }
+
+  enterShip(ship) {
+    this.ship = ship;
+    ship.crew.push(this)
+    // console.log(ship.crew.push(this))
+  }
 
 
+}
 
+class Ship {
+  constructor(name, type, ability) {
+    this.name =name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+  
+  } 
+  missionStatement(){
+
+  //if it is only a ship name 
+  // ship.name = mav or ship.name === "hermes"  ==> 
+  // "Can't perform a mission yet."
+    if(this.crew.length === 0){ // if the crew is blank
+      return "Can't perform a mission yet."
+    }
+
+    //if there is a crew attach to the function entership and it's name is mav or hermes
+  // crewMember1.enterShip(ship.name = mav) or  crewMember2.enterShip(hermes) ===> Ascend into low orbit
+    else {
+      return this.ability
+    }
+  
+  }
+
+}
+
+let Ying = new CrewMember("Ying", "Pilot", "Coding")
+let Sparkler = new CrewMember("Sparkler", "Pilot", "Drawing")
+console.log(Ying)
+let Ninja = new Ship('Ninja', 'Ninja Ship', 'ET Travel'); //ship(name, type, ability)
+Ying.enterShip(Ninja)
+Sparkler.enterShip(Ninja)
+console.log(Ying)
+console.log(Ninja)
+console.log(Ninja.crew)
+console.log(Ninja.missionStatement())
 
 
 
@@ -53,10 +104,11 @@ if (typeof describe === 'function'){
     });
 
     it('can return a mission statement correctly', function(){
-      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
-      const crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
-      let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
-      const crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
+      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit'); //ship(name, type, ability)
+      const crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry'); //crewmember(name, job, special skills)
+      let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel'); //ship(name, type, ability)
+      
+      const crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');//crewmember(name, job, special skills)
       assert.equal(mav.missionStatement(), "Can't perform a mission yet.");
       assert.equal(hermes.missionStatement(), "Can't perform a mission yet.");
 
